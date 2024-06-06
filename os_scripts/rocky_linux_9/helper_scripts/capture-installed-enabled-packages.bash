b785d6b6211191793bc8b77cbed6a1d76371f0ba
patilameya92 <patilameya92@gmail.com>
2024-06-05 22:19:18 -0400

    uncomplicated awk usage for systemctl listing

--- os_scripts/rocky_linux_9/helper_scripts/capture-installed-enabled-packages.bash
+++ os_scripts/rocky_linux_9/helper_scripts/capture-installed-enabled-packages.bash
@@ -7,10 +7,10 @@ CAPTURE_DIRECTORY="${CAPTURE_DIRECTORY_PREFIX}-${CURRENT_TIME}"
 mkdir -p "${CAPTURE_DIRECTORY}"
 
 # Capture installed packages
-dnf list installed | awk '{print $1}' > "${CAPTURE_DIRECTORY}/installed-packages.txt"
+dnf list installed | tail -n +2 | awk '{print $1}' > "${CAPTURE_DIRECTORY}/installed-packages.txt"
 
 # Capture systemctl enabled services
-systemctl list-unit-files --state=enabled | awk 'NR > 1 && $1 != "" {print $1}' | head -n -1 > "${CAPTURE_DIRECTORY}/enabled-services.txt"
+systemctl list-unit-files --state=enabled | tail -n +2 | head -n -2 | awk '{print $1}' > "${CAPTURE_DIRECTORY}/enabled-services.txt"
 
 # Capture running services
-systemctl list-units --type=service --state=running | awk 'NR > 1 && $1 != "" {print $1}' | head -n -4 > "${CAPTURE_DIRECTORY}/running-services.txt"
+systemctl list-units --type=service --state=running | tail -n +2 | head -n -5 | awk '{print $1}' > "${CAPTURE_DIRECTORY}/running-services.txt"
